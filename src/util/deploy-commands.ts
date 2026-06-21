9863f7dcd77c52c008de729a90dcd5e2c4344a13
kayskayskays <kays6061@gmail.com>
2026-06-21 23:49:52 +1000

    deploy-commands.ts now accepts a command line argument for guild id

--- src/util/deploy-commands.ts
+++ src/util/deploy-commands.ts
@@ -2,9 +2,14 @@ import "dotenv/config";
 import { CommandRegistry } from "../commands/CommandRegistry.js";
 import { CMDS } from "./all-commands.js";
 
+const [guildId, ...extraArgs] = process.argv.slice(2);
+if ( guildId === undefined || extraArgs.length > 0 ) {
+    console.error("Usage: haikubot-deploy <guild-id>");
+    process.exit(1);
+}
+
 const token = process.env.DISCORD_TOKEN!;
 const clientId = process.env.CLIENT_ID!;
-const guildId = process.env.GUILD_ID!;
 
 const registry = new CommandRegistry(CMDS);
 await registry.deployAll(token, clientId, guildId).catch(_ => process.exit(1));
